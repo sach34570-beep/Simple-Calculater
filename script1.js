3e42b74865ad29ff5176e13b81d2447d9ed6067e
Sachin Soni <sach34570@gmail.com>
2026-02-04 09:30:23 -0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -13,4 +13,26 @@ function calculate(){
     } catch (error) {
         display.value = "Error";
     }
+
+}
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+display.value = eval(display.value);
+function calculate() {
+    try {
+        let result = eval(display.value);
+
+        if (result === Infinity || result === -Infinity) {
+            display.value = "∞";
+        } 
+        else if (isNaN(result)) {
+            display.value = "Error";
+        } 
+        else {
+            display.value = result;
+        }
+    } catch (error) {
+        display.value = "Error";
+    }
 }
